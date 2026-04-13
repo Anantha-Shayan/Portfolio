7b17f31a03ba734432058c5b7a1bb8add096ef8f
Anantha Shayan <ananthashayan2802@outlook.com>
2026-04-13 21:47:42 +0530

    Added AnalyticsTracker.tsx

---
+++ components/AnalyticsTracker.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+
+export default function AnalyticsTracker() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (typeof window.gtag === 'function') {
+      window.gtag('config', 'G-XXXXXXXXXX', {
+        page_path: pathname,
+      });
+    }
+  }, [pathname]);
+
+  return null;
+}
